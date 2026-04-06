f87cf7b71f9012e31a1c67b052488c03bf14f95c
markusx5622 <cooperdan598@gmail.com>
2026-04-06 11:09:54 +0200

    Create typings.d.ts

---
+++ typings.d.ts
@@ -0,0 +1,4 @@
+declare module 'maath/random/dist/maath-random.esm' {
+  export function inSphere(array: Float32Array, args?: { radius?: number }): Float32Array;
+  export function inBox(array: Float32Array, args?: { side?: number }): Float32Array;
+}
